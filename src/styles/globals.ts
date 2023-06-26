4ba1f789e21956971ea854d87b35ee2b3cf8c273
Sávio Almeida <savio.pinheiroal@gmail.com>
2023-06-26 16:14:50 -0300

    add GlobalStyle at app

---
+++ src/styles/globals.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+
+   html {
+      font-size: 10px;
+
+      body {
+         min-height: 100vh;
+         scroll-behavior: smooth;
+         margin: 0;
+         padding: 0;
+         box-sizing: border-box;
+         overflow-x: hidden;
+         max-width: 100vw;
+         overflow-x: hidden;
+         color: red;
+      }
+   }
+`;
+
+export default GlobalStyle;
