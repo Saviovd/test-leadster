8e206d8e480bea9ce0845c896d9d026054b637b0
Sávio Almeida <savio.pinheiroal@gmail.com>
2023-06-28 12:43:35 -0300

    add responsiveness to Call component

--- src/components/Call/Call.tsx
+++ src/components/Call/Call.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { CallStyle } from './callStyle';
 
 const Call = () => {
@@ -5,10 +6,19 @@ const Call = () => {
       <CallStyle>
          <span className='exclusive__webnar'>webnars exclusivos</span>
          <h3 className='subtitle'>menos conversinha,</h3>
-         <h1 className='title'>mais conversão</h1>
+         <h1 className='title'>
+            mais conversão
+            <Image
+               src={'/assets/asset-header.png'}
+               alt='header logo'
+               width={50}
+               height={50}
+               className='header__logo'
+            />
+         </h1>
          <p className='call_to_action'>
-            Conheça as estratégias que <b>mudaram o jogo</b> e como aplicá-las no seu
-            negócio
+            Conheça as estratégias que <b>mudaram o jogo</b> e como aplicá-las
+            no seu negócio
          </p>
       </CallStyle>
    );
--- src/components/Call/callStyle.ts
+++ src/components/Call/callStyle.ts
@@ -7,11 +7,11 @@ export const CallStyle = styled.section`
    justify-content: center;
 
    background-color: #f0f8ff;
-   height: 80vh;
+   height: 70vh;
+   min-height: 37rem;
    max-height: 70rem;
 
    .exclusive__webnar {
-
       color: #007eff;
       font-size: 1.5rem;
       text-transform: uppercase;
@@ -20,14 +20,56 @@ export const CallStyle = styled.section`
       border: 2px solid #007eff;
       border-radius: 1.5rem 1.5rem 1.5rem 0.3rem;
       padding: 0.7rem 2rem;
+      @media screen {
+         @media (max-width: 768px) {
+            font-size: 1.3rem;
+         }
+         @media (max-width: 600px) {
+            font-size: 1.2rem;
+         }
+         @media (max-width: 540px) {
+            font-size: 1.1rem;
+            padding: 0.5rem 1.5rem;
+         }
+         @media (max-width: 460px) {
+            font-size: 1rem;
+            padding: 0.3rem 1rem;
+         }
+         @media (max-width: 420px) {
+            font-size: 0.9rem;
+         }
+         @media (max-width: 370px) {
+            font-size: 0.8rem;
+         }
+      }
    }
    .subtitle {
       color: #1c3c50;
       font-size: 4rem;
       font-weight: 500;
       text-transform: capitalize;
-      margin: .5rem 0 0;
+      margin: 0.5rem 0 0;
       letter-spacing: 0.2rem;
+      @media screen {
+         @media (max-width: 768px) {
+            font-size: 3.5rem;
+         }
+         @media (max-width: 600px) {
+            font-size: 3rem;
+         }
+         @media (max-width: 540px) {
+            font-size: 2.7rem;
+         }
+         @media (max-width: 460px) {
+            font-size: 2.3rem;
+         }
+         @media (max-width: 420px) {
+            font-size: 2rem;
+         }
+         @media (max-width: 370px) {
+            font-size: 1.8rem;
+         }
+      }
    }
    .title {
       font-size: 8rem;
@@ -43,19 +85,100 @@ export const CallStyle = styled.section`
       );
       -webkit-background-clip: text;
       -webkit-text-fill-color: transparent;
-      padding: 0 3rem;
+      padding: 0 2.5rem 0;
       border-bottom: 0.1px solid #c8d4dd;
       position: relative;
 
-      &:after {
-         content: url('assets/asset-header.png');
+      .header__logo {
+         object-fit: contain;
          position: absolute;
-         top: -3.8rem;
-         right: 1rem;
+         transform: translate(-3rem, 0.1rem);
+         @media screen {
+            @media (max-width: 768px) {
+               width: 4rem;
+               height: 4rem;
+               transform: translate(-2.3rem, 0.5rem);
+            }
+            @media (max-width: 600px) {
+               width: 3.5rem;
+               height: 3.5rem;
+               transform: translate(-2rem, 0.5rem);
+            }
+            @media (max-width: 540px) {
+               width: 3.5rem;
+               height: 3.5rem;
+               transform: translate(-1.9rem, 0rem);
+            }
+            @media (max-width: 460px) {
+               width: 2.8rem;
+               height: 2.8rem;
+               transform: translate(-1.6rem, 0.15rem);
+            }
+            @media (max-width: 420px) {
+               width: 2.4rem;
+               height: 2.4rem;
+               transform: translate(-1.4rem, 0.25rem);
+            }
+            @media (max-width: 370px) {
+               width: 2.2rem;
+               height: 2.2rem;
+               transform: translate(-1.2rem, 0.25rem);
+            }
+         }
+      }
+
+      @media screen {
+         @media (max-width: 768px) {
+            font-size: 6.8rem;
+         }
+         @media (max-width: 600px) {
+            font-size: 6rem;
+         }
+         @media (max-width: 540px) {
+            font-size: 5rem;
+         }
+         @media (max-width: 460px) {
+            font-size: 4.5rem;
+         }
+         @media (max-width: 420px) {
+            font-size: 4rem;
+         }
+         @media (max-width: 370px) {
+            font-size: 3.6rem;
+            padding: 0 1rem 0;
+         }
       }
    }
    .call_to_action {
       font-size: 1.6rem;
       font-weight: 500;
+
+      @media screen {
+         @media (max-width: 768px) {
+            font-size: 1.35rem;
+         }
+         @media (max-width: 600px) {
+            font-size: 1.2rem;
+         }
+         @media (max-width: 540px) {
+            font-size: 1.1rem;
+         }
+         @media (max-width: 460px) {
+            font-size: 1rem;
+         }
+         @media (max-width: 420px) {
+            font-size: 0.9rem;
+         }
+         @media (max-width: 370px) {
+            font-size: 0.8rem;
+         }
+      }
+   }
+
+   @media screen {
+      @media (max-width: 460px) {
+         height: 60vh;
+         min-height: 30rem;
+      }
    }
 `;
